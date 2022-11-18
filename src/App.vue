bdfc85035cfd023b4c5678ee82894a60cd35c0b0
179Bell <shingo.msdf@gmail.com>
2022-11-18 18:13:47 +0900

    vue2でプロジェクト再作成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
-<template lang="">
+<template>
   <div id="app">
-    <Header/>
-    <Footer/>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Header from "@/global/Header.vue";
-import Footer from "@/global/Footer.vue";
-
-export default {
-  components: {
-    Header,
-    Footer
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
