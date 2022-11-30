11c3c9f2148c6228531c68cc242f3f6d983ae2aa
179Bell <shingo.msdf@gmail.com>
2022-11-30 21:59:58 +0900

    編集画面実装

--- src/pages/Edit.vue
+++ src/pages/Edit.vue
@@ -1,13 +1,30 @@
-<template lang="">
+<template>
     <div>
-        
+        <v-row>
+            <v-col cols="12">
+                <v-card class="mx-auto">
+                    <v-row>
+                        <v-col cols="4">
+                            <v-img :src="logs.image"></v-img>
+                        </v-col>
+                        <v-col cols="8">
+                            <v-card-title>{{ logs.name }}</v-card-title>
+                        </v-col>
+                    </v-row>
+                    <v-card-actions>
+                        <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+                        <v-btn color="info">更新する</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
 </template>
 <script>
 export default {
-    
+    name: 'Edit',
+    props: {
+        logs: Array
+    }
 }
 </script>
-<style lang="">
-    
-</style>
